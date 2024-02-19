bf9d2915c2b3fa23d4203ad71b9997909a2055a8
Edgar Santos <edgar.a2407@gmail.com>
2024-02-18 19:15:09 -0600

    Cambios a archivos html, CSS y JS , se arregló el boton y variables en JS y opciones a la opción desplegable

--- js/main.js
+++ js/main.js
@@ -1,48 +1,74 @@
+
+
 let txtNombre = document.getElementById("Nombre");
 let txtTelefono = document.getElementById("Telefono");
 let txtCorreo = document.getElementById("Correo");
 let btnEnviar = document.getElementById("btnEnviar");
-var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+let txtInput = document.getElementById("TextInput");
+
+
+let alertValidaciones = document.getElementById("alertValidaciones");
+let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+
+var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 //alerta Validación NATASHA
 
-btnEnviar.addEventListener("click", function(event) {
+btnEnviar.addEventListener("click", function (event) {
     event.preventDefault();
-    if (!validarNombre()) {
-        //alert("El nombre debe tener al menos 3 caracteres."); NATASHA
-        return;
+
+    if (txtNombre.value.length < 3) {
+        alertValidacionesTexto.insertAdjacentHTML("beforeend", `El <strong>Nombre </strong> ingresado no es correcto <br/>`);
+        alertValidaciones.syle.display = "block";
+        txtNombre.style.border = "solid red thin";t
+        isValid = false;
     }
 
-    if (!validarCorreo(txtCorreo.value)) {
-        //alert("El correo electrónico no es válido."); NATASHA
-        return;
+    //alert("El nombre debe tener al menos 3 caracteres."); NATASHA
+    if (txtCorreo.value.length < 3) {
+        alertValidacionesTexto.insertAdjacentHTML("beforeend", `El <strong>Correo electrónico </strong> ingresado no es correcto <br/>`);
+        alertValidaciones.style.display = "block";
+        txtNombre.style.border = "solid red thin";
+        isValid = false;
     }
+    //alert("El correo electrónico no es válido."); NATASHA
 
-    if (!validarTelefono()) {
-        //alert("El teléfono debe tener 10 dígitos numéricos."); NATASHA
-        return;
+    if (txtTelefono.value.length < 10) {
+        alertValidacionesTexto.insertAdjacentHTML("beforeend", `El <strong>Número telefónico </strong> ingresado no es correcto <br/>`);
+        alertValidaciones.style.display = "block";
+        txtNombre.style.border = "solid red thin";
+        isValid = false;
     }
+    //alert("El teléfono debe tener 10 dígitos numéricos."); NATASHA
+
 });
 
 
-function validarNombre(){
-    return txtNombre.value.length >=3;
+function validarNombre() {
+    return txtNombre.value.length >= 3;
 }//if length Nombre
 
-function validarCorreo(correo){
+function validarCorreo(correo) {
     return expReg.test(correo);
 }//validarCorreo
 
 txtTelefono.value = txtNombre.value.trim();
-function validarTelefono(){
-    if (txtTelefono.value.length<10){
+function validarTelefono() {
+    if (txtTelefono.value.length < 10) {
         return false;
     }//if length
-    if (isNaN(txtTelefono.value)){
+    if (isNaN(txtTelefono.value)) {
         return false
     }//isNaN
-    if (Number(txtTelefono.value)<=0){
+    if (Number(txtTelefono.value) <= 0) {
         return false
     }//if
     return true;
+
+
 }//validarCantidad
+
+function validarMensaje() {
+    return txtInput.value.length >= 10;
+}//if length Nombre
+ 
 
