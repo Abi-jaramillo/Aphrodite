2f0446d20ef487480b6a0f0e6cd393798c285c4a
carina <carina_jaramillo99uwu@ciencias.unam.mx>
2024-02-22 16:47:12 -0600

    Se agregó el footer en javascript y se agregó un diseño inicial

---
+++ js/footer.js
@@ -0,0 +1,19 @@
+let footer = document.getElementById("footer");
+footer.insertAdjacentHTML("beforebegin", `<div class="footer-basic">
+<footer>
+    <div class="social">
+    <a href="#"><i class="bi bi-instagram"></i></a>
+    <a href="#"><i class="bi bi-tiktok"></i></a>
+    <a href="#"><i class="bi bi-whatsapp"></i></a>
+    <a href="#"><i class="bi bi-facebook"></i></a>
+    </div>
+    <ul class="list-inline">
+        <li class="list-inline-item"><a href="/HTML/principal.html">Inicio</a></li>
+        <li class="list-inline-item"><a href="/HTML/tienda.html">Tienda</a></li>
+        <li class="list-inline-item"><a href="/HTML/acercadenosotros.html">Nosotros</a></li>
+        <li class="list-inline-item"><a href="/HTML/contactanos.html">Contáctanos</a></li>
+        <li class="list-inline-item"><a href="/HTML/reseñas.html">Reseñas</a></li>
+    </ul>
+    <p class="copyright">Aphrodite © 2024</p>
+</footer>
+</div>`)
