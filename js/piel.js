1dc23e8ff745e2fdf3e9316ff38ed3dccfede43b
Lyanthus <jsh.ibarra@gmail.com>
2024-02-27 13:44:54 -0600

    Se crearon los 3 archivos de js y se agregó el código de BS para las cards

---
+++ js/piel.js
@@ -0,0 +1,21 @@
+
+function addItem(item) {
+    const itemHTML = `<div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`;
+    const itemsContainer = document.getElementById("list-items");
+    itemsContainer.innerHTML += itemHTML;
+}
+
+addItem({'name':'juice',
+    'img':'https://www.gs1india.org/media/Juice_pack.jpg',
+    'description':'Orange and Apple juice fresh and delicious'});
+
+addItem({'name':'Tayto',
+    'img':'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg',
+    'description':'Cheese & Onion Chips'})
