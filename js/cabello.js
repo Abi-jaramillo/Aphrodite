c0eb5b5c404ff46cb152f16a621bc11cfe64a12f
Nadia Martínez <naddmtzlo@outlook.com>
2024-02-27 18:17:22 -0600

    Se quitó el bg de la navbar, se agregaron productos de cabello

--- js/cabello.js
+++ js/cabello.js
@@ -1,21 +1,35 @@
-
-function addItem(item){
-    const itemHTML = `<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>`;
-    const itemsContainer = document.getElementById("list-items");
-    itemsContainer.innerHTML += itemHTML;
+function addItem(item) {
+  const itemHTML = `<div class="card" style="width: 18rem;">
+  <img src="`+item.img +`" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">`+item.name+`</h5>
+    <p class="card-text">`+item.description+`</p>
+    <p class="card-text">`+item.price+`</p>
+    <a href="#" class="btn btn-primary">Agregar al carrito</a>
+  </div>
+</div>`;
+  const itemsContainer = document.getElementById("list-items");
+  itemsContainer.innerHTML += itemHTML;
 }
 
-addItem({'name':'juice',
-    'img':'https://www.gs1india.org/media/Juice_pack.jpg',
-    'description':'Orange and Apple juice fresh and delicious'});
 
-addItem({'name':'Tayto',
-    'img':'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg',
-    'description':'Cheese & Onion Chips'})
+addItem({'name':'Shampoo',
+    'img':'https://www.thebodyshop.mx/cdn/shop/products/TBS_ShampooTeaTree_250ml_Haircare_1800x1800.jpg?v=1670532464',
+    'description':'Este Shampoo deja el cabello y el cuero cabelludo grasos purificado, vigorizado y con sensación de limpieza profunda. Contribuye a reequilibrar el exceso de grasa sin retirar la hidratación esencial del cabello',
+    'price':'$260'});
+
+addItem({'name':'Acondicionador',
+    'img':'https://www.thebodyshop.mx/cdn/shop/products/Acondicionador_sin_enjuague_deAceite_de_RicinoNegro_Jamaicano_1_1800x1800.jpg?v=1678826719',
+    'description':'Esta crema hidratante suave pero intensa es justo lo que necesitas, ya que repone la humedad y reduce el encrespamiento',
+    'price':'$260'});
+
+    addItem({'name':'Mascarilla',
+    'img':'https://www.thebodyshop.mx/cdn/shop/products/TBS_MascarillaCapilarPlatano_240ml_Haircare_1800x1800.jpg?v=1670956529',
+    'description':' nutre intensamente el cabello de la raíz a las puntas, dejándolo más brillante al instante, combatiendo el frizz sin apelmazar el cabello',
+    'price':'$180'});
+    
+    addItem({'name':'Cepillo',
+    'img':'https://www.thebodyshop.mx/cdn/shop/products/TBS_CepilloCuadradoDeBambooGrande_Accesorios_1800x1800.jpg?v=1667254507',
+    'description':'Cepillo ovalado de bambú grande',
+    'price':'$90'});
+
--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 let navbar = document.getElementById("navbar");
 navbar.insertAdjacentHTML("beforeend", `
-    <nav class= "navbar navbar-expand-lg bg-body-tertiary">
+    <nav class= "navbar navbar-expand-lg bg-transparent">
     <div class="container-fluid">
         <a class="navbar-brand" href="/HTML/principal.html">Aphrodite</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,7 +12,7 @@ navbar.insertAdjacentHTML("beforeend", `
                 <button class="btn btn-outline-success" type="submit">Buscar</button>
             </form>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="lista">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 bg-transparent" id="lista">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="/src/">Inicio</a>
                 </li>
