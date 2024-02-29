8237a1e8f904b1cfbf6d2272a1674c741db83f62
Nadia Martínez <naddmtzlo@outlook.com>
2024-02-28 19:11:10 -0600

    Se solucionaron los problemas de enlazamiento en la navbar y el footer, se cambió la página principal.html por index.html

--- js/footer.js
+++ js/footer.js
@@ -9,11 +9,10 @@ footer.insertAdjacentHTML("beforebegin",
     <a href="#"><i class="bi bi-facebook"></i></a>
     </div>
     <ul class="list-inline">
-        <li class="list-inline-item"><a href="/HTML/principal.html">Inicio</a></li>
-        <li class="list-inline-item"><a href="/HTML/tienda.html">Tienda</a></li>
-        <li class="list-inline-item"><a href="/HTML/acercadenosotros.html">Nosotros</a></li>
-        <li class="list-inline-item"><a href="/HTML/contactanos.html">Contáctanos</a></li>
-        <li class="list-inline-item"><a href="/HTML/reseñas.html">Reseñas</a></li>
+        <li class="list-inline-item"><a href="../HTML/index.html">Inicio</a></li>
+        <li class="list-inline-item"><a href="../HTML/tienda.html">Tienda</a></li>
+        <li class="list-inline-item"><a href="../HTML/acercadenosotros.html">Nosotros</a></li>
+        <li class="list-inline-item"><a href="../HTML/contactanos.html">Contáctanos</a></li>
     </ul>
     <p class="copyright">Aphrodite © 2024</p>
     </footer>
--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@ let navbar = document.getElementById("navbar");
 navbar.insertAdjacentHTML("beforeend", `
     <nav class= "navbar navbar-expand-lg bg-transparent">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/HTML/principal.html">Aphrodite</a>
+        <a class="navbar-brand" href="../HTML/index.html">Aphrodite</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -14,29 +14,22 @@ navbar.insertAdjacentHTML("beforeend", `
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 bg-transparent" id="lista">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/src/">Inicio</a>
+                    <a class="nav-link active" aria-current="page" href="../HTML/index.html">Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/HTML/tienda.html">Tienda</a>
+                    <a class="nav-link active" aria-current="page" href="../HTML/tienda.html">Tienda</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/HTML/contactanos.html">Contáctanos</a>
+                    <a class="nav-link active" aria-current="page" href="../HTML/contactanos.html">Contáctanos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Reservaciones</a>
+                    <a class="nav-link active" aria-current="page" href="../HTML/acercadenosotros.html">Nosotros</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/HTML/reseñas.html">Reseñas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/HTML/acercadenosotros.html">Nosotros</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Blog</a>
+                    <a class="nav-link active" aria-current="page" href="../HTML/blog.html">Blog</a>
                 </li>
                 
                 <a href="#" target="_blank"><i id="carrito1" class="bi bi-cart3"></i></a>
-              
             </ul>
             
         </div>
