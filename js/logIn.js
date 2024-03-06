2b00f1b482e873c23c5f331a86d6e988328c6878
carina <carina_jaramillo99uwu@ciencias.unam.mx>
2024-03-05 20:14:46 -0600

    Se agregó la validación de las contraseñas y sus alertas

--- js/logIn.js
+++ js/logIn.js
@@ -3,28 +3,39 @@ let txtTelefono = document.getElementById("Telefono");
 let txtCorreo = document.getElementById("Correo");
 let btnRegistrarse = document.getElementById("btnRegistrarse");
 let btnClear = document.getElementById ("btnClear");
+let txtContrasena1 = document.getElementById("Contrasena1");
+let txtContrasena2 = document.getElementById("Contrasena2");
 
 let alertValidaciones = document.getElementById("alertValidaciones");
 let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
 
+let alertConfirmaciones = document.getElementById("alertConfirmaciones");
+let alertConfirmacionesTexto = document.getElementById("alertConfirmacionesTexto");
+
 //Se agregaron las excepciones de simbolos que no deben estar en correo
 let expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 let regex = new RegExp ("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$")
-
+let regexContraseña = (/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[$@$!%?&])([A-Za-z\d$@$!%?&]|[^ ]){8,15}$/)
 //alerta Validación 
 btnRegistrarse.addEventListener("click", function (event) {
     event.preventDefault();
 
     alertValidacionesTexto.innerHTML="";
     alertValidaciones.style.display="none";
+    alertConfirmacionesTexto.innerHTML="";
+    alertConfirmaciones.style.display="none";
 
     txtNombre.value = txtNombre.value.trim();
     txtTelefono.value = txtTelefono.value.trim();
     txtCorreo.value = txtCorreo.value.trim();
+    txtContrasena1.value= txtContrasena1.value.trim();
+    txtContrasena2.value= txtContrasena2.value.trim();
 
     txtNombre.style.border="";
     txtTelefono.style.border="";
     txtCorreo.style.border="";
+    txtContrasena1.style.border="";
+    txtContrasena2.style.border="";
 
     let hayError = false;
 
@@ -52,10 +63,35 @@ btnRegistrarse.addEventListener("click", function (event) {
         hayError=true;
         
     } 
-        //Se usa el boton de enviar con estos campos requeridos
+
+    if((txtContrasena1.value.lenght<8) && !regexContraseña.test(txtContrasena1.value)){
+        alertValidacionesTexto.insertAdjacentHTML("beforeend", `La <strong> Contraseña </strong> 
+        debe tener: <br> -Mínimo <strong>8</strong> caracteres y máximo <strong>15</strong>. <br>
+        -Una letra mayúscula y mínimo una minuscula. <br> -Al menos un número y un carácter especial. <br>`);
+        alertValidaciones.style.display = "block";
+        txtContrasena1.style.border = "solid red thin";
+        hayError=true;
+    }
+
+    if(txtContrasena1.value !=txtContrasena2.value){
+        alertValidacionesTexto.insertAdjacentHTML("beforeend", `Las <strong> Contraseñas </strong> 
+        no coinciden <br>`);
+        alertValidaciones.style.display = "block";
+        txtContrasena1.style.border = "solid red thin";
+        txtContrasena2.style.border = "solid red thin";
+        hayError=true;
+    } else { alertConfirmacionesTexto.insertAdjacentHTML("beforeend", `Las <strong> Contraseñas </strong> coinciden <br>`);
+        alertConfirmaciones.style.display = "block";
+        txtContrasena1.style.border = "solid green thin";
+        txtContrasena2.style.border = "solid green thin";
+        hayError=false;
+    }
+
                 txtNombre.value="";
                 txtTelefono.value="";
                 txtCorreo.value="";
+                txtContrasena1.value="";
+                txtContrasena2.value="";
 
         txtNombre.focus();
 
@@ -67,11 +103,18 @@ btnClear.addEventListener("click", function(event){
     txtNombre.value="";
     txtTelefono.value="";
     txtCorreo.value="";
+    txtContrasena1.value="";
+    txtContrasena2.value="";
     alertValidacionesTexto.innerHTML="";
     alertValidaciones.style.display="none";
+    alertConfirmacionesTexto.innerHTML="";
+    alertConfirmaciones.style.display="none";
     txtNombre.style.border="";
     txtTelefono.style.border="";
     txtCorreo.style.border="";
+    txtContrasena1.style.border="";
+    txtContrasena2.style.border="";
+
 });
 
 
@@ -82,6 +125,7 @@ function validarNombre() {
 function validarCorreo(correo) {
     return expReg.test(correo);
 }//validarCorreo
+
 
 
  
