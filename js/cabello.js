0d5bd09776cdcbafda5b6d7043403a12f87b5e47
Natasha <natashasosa1012@gmail.com>
2024-03-19 19:32:29 -0600

    Se ajustó la responsividad del carrusel en la página principal y se agregaron 3 productos para la barba en la sección para cabello

--- js/cabello.js
+++ js/cabello.js
@@ -19,51 +19,86 @@ function addItem(item) {
 }
 
 
-addItem({'name':'Kit Pro-Reconstrucción',
-    'section':'Cabello',
-    'img':'https://production.na01.natura.com/on/demandware.static/-/Sites-nat-mex-b2b2c-catalog/default/dw49370b91/6070_1.jpg',
-    'description':'Kit para cabellos químicamente dañados. Proporciona fuerza y resistencia, además de que ofrece un reequilibrio de la fibra capilar',
-    'price':'$220'});
+addItem({
+  'name': 'Kit Pro-Reconstrucción',
+  'section': 'Cabello',
+  'img': 'https://production.na01.natura.com/on/demandware.static/-/Sites-nat-mex-b2b2c-catalog/default/dw49370b91/6070_1.jpg',
+  'description': 'Kit para cabellos químicamente dañados. Proporciona fuerza y resistencia, además de que ofrece un reequilibrio de la fibra capilar',
+  'price': '$220'
+});
 
-addItem({'name':'Shampoo 2 en 1',
-    'section':'Cabello',
-    'img':'https://production.na01.natura.com/on/demandware.static/-/Sites-nat-mex-b2b2c-catalog/default/dw27bd57e1/78904_1.jpg',
-    'description':'Shampoo 2 en 1 ideal para uso diario y todo tipo de cabello. Contiene manteca de cacao y complejos vitamínicos',
-    'price':'$130'});
+addItem({
+  'name': 'Shampoo 2 en 1',
+  'section': 'Cabello',
+  'img': 'https://production.na01.natura.com/on/demandware.static/-/Sites-nat-mex-b2b2c-catalog/default/dw27bd57e1/78904_1.jpg',
+  'description': 'Shampoo 2 en 1 ideal para uso diario y todo tipo de cabello. Contiene manteca de cacao y complejos vitamínicos',
+  'price': '$130'
+});
 
-addItem({'name':'Shampoo hidratante',
-    'section':'Cabello',
-    'img':'https://www.thebodyshop.mx/cdn/shop/products/TBS_ShampooTeaTree_250ml_Haircare_1800x1800.jpg?v=1670532464',
-    'description':'Purifica tu cabello con este shampoo dejando una sensación de limpieza profunda. Ayuda a reequilibrar el exceso de grasa sin modificar la hidratación esencial del cabello',
-    'price':'$260'});
+addItem({
+  'name': 'Shampoo hidratante',
+  'section': 'Cabello',
+  'img': 'https://www.thebodyshop.mx/cdn/shop/products/TBS_ShampooTeaTree_250ml_Haircare_1800x1800.jpg?v=1670532464',
+  'description': 'Purifica tu cabello con este shampoo dejando una sensación de limpieza profunda. Ayuda a reequilibrar el exceso de grasa sin modificar la hidratación esencial del cabello',
+  'price': '$260'
+});
 
-addItem({'name':'Acondicionador',
-    'section':'Cabello',
-    'img':'https://www.thebodyshop.mx/cdn/shop/products/Acondicionador_sin_enjuague_deAceite_de_RicinoNegro_Jamaicano_1_1800x1800.jpg?v=1678826719',
-    'description':'Descubre el secreto para un cabello radiante, suave y sedoso con nuestro acondicionador. Revitaliza, fortalece y elimina el frizz',
-    'price':'$260'});
+addItem({
+  'name': 'Acondicionador',
+  'section': 'Cabello',
+  'img': 'https://www.thebodyshop.mx/cdn/shop/products/Acondicionador_sin_enjuague_deAceite_de_RicinoNegro_Jamaicano_1_1800x1800.jpg?v=1678826719',
+  'description': 'Descubre el secreto para un cabello radiante, suave y sedoso con nuestro acondicionador. Revitaliza, fortalece y elimina el frizz',
+  'price': '$260'
+});
 
-addItem({'name':'Mascarilla',
-    'section':'Cabello',
-    'img':'https://www.thebodyshop.mx/cdn/shop/products/TBS_MascarillaCapilarPlatano_240ml_Haircare_1800x1800.jpg?v=1670956529',
-    'description':' Nutre intensamente el cabello desde la raíz hasta las puntas, combatiendo el frizz y dejándolo más brilloso y manejable',
-    'price':'$180'});
-    
-addItem({'name':'Cepillo',
-    'section':'Cabello',
-    'img':'https://www.thebodyshop.mx/cdn/shop/products/TBS_CepilloCuadradoDeBambooGrande_Accesorios_1800x1800.jpg?v=1667254507',
-    'description':'Cepillo de cerdas gruesas fabricado a base de bambú',
-    'price':'$90'});
+addItem({
+  'name': 'Mascarilla',
+  'section': 'Cabello',
+  'img': 'https://www.thebodyshop.mx/cdn/shop/products/TBS_MascarillaCapilarPlatano_240ml_Haircare_1800x1800.jpg?v=1670956529',
+  'description': ' Nutre intensamente el cabello desde la raíz hasta las puntas, combatiendo el frizz y dejándolo más brilloso y manejable',
+  'price': '$180'
+});
+
+addItem({
+  'name': 'Cepillo',
+  'section': 'Cabello',
+  'img': 'https://www.thebodyshop.mx/cdn/shop/products/TBS_CepilloCuadradoDeBambooGrande_Accesorios_1800x1800.jpg?v=1667254507',
+  'description': 'Cepillo de cerdas gruesas fabricado a base de bambú',
+  'price': '$90'
+});
+
+addItem({
+  'name': 'Cepillo para barba',
+  'section': 'Cabello',
+  'img': 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSb0xYCGr5ZKm7ZKHBiwY0RPVzS3XnTwKkTBHTJ_jMExiEmNuiKDVBlPVTD4NDk1FTjoxx5I0Hvgcp9ccHWuZPHvpJBOfJpC4OOnXRYDJdFJKirHONJMqew&usqp=CAE',
+  'description': 'Cepillo para barba con cerdas 100% naturales de pelo de jabalí',
+  'price': '$199'
+});
+addItem({
+  'name': 'Aceite para barba',
+  'section': 'Cabello',
+  'img': 'https://sallybeautymx.vtexassets.com/arquivos/ids/181887-1200-1200?v=637496582203270000&width=1200&height=1200&aspect=true',
+  'description': 'El aceite de argán para barba dejará tu barba suave y la piel bien hidratada',
+  'price': '$126'
+});
+addItem({
+  'name': 'Kit para afeitar para barba',
+  'section': 'Cabello',
+  'img': 'https://production.na01.natura.com/on/demandware.static/-/Sites-nat-mex-b2b2c-catalog/default/dw56ff8d59/147180.jpg',
+  'description': 'Con el kit para afeitar de natura mejora el desliz de la navaja mientras refrescas e hidratas de piel de tu rostro.',
+  'price': '$329'
+});
 
 
-  
-let mainProd= document.getElementById("list-itemsHair");    
-window.addEventListener("load",function(event){
+
+
+let mainProd = document.getElementById("list-itemsHair");
+window.addEventListener("load", function (event) {
   event.preventDefault();
-  if(this.localStorage.getItem("datos")!=null){
-      datos = JSON.parse(this.localStorage.getItem("datos"));
-      datos.forEach((r) => {
-       let= card = `<div class=col> <div class="card">
+  if (this.localStorage.getItem("datos") != null) {
+    datos = JSON.parse(this.localStorage.getItem("datos"));
+    datos.forEach((r) => {
+      let = card = `<div class=col> <div class="card">
        <img src=${r.img} class="card-img-top" alt="...">
        <div class="card-body">
          <h5 class="card-title">${r.name}</h5>
@@ -76,10 +111,10 @@ window.addEventListener("load",function(event){
      </div> 
      </div> 
      </br>`;
-     if(r.section === '1'){
-      mainProd.insertAdjacentHTML("beforeend",card);
-  };
-     
-  });//forEach
-};//if
+      if (r.section === '1') {
+        mainProd.insertAdjacentHTML("beforeend", card);
+      };
+
+    });//forEach
+  };//if
 });
